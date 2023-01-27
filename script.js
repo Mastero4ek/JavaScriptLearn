c86650927cc188d1f0eb4cd1581748a1653f5d04
Chirkov Slava <slavachirkov92@gmail.com>
2023-01-27 14:20:06 +0300

    выполнил задание 02

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const title = "title",
-	  screens = "screens",
-	  screenPrice = "screen price",
-	  rollback = "rollback",
-	  fullPrice = "full price",
-	  adaptive = "adaptive";
+const title = "Разработка сайтов!",
+	  screens = "Простые, Сложные, Интерактивные",
+	  screenPrice = 25,
+	  rollback = 75,
+	  fullPrice = 135600,
+	  adaptive = true;
 
-alert(screenPrice);
-
-console.log(fullPrice);
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
+console.log(screens.toLowerCase().split(", "));
+console.log(`Процент отката посреднику за работу (${fullPrice} * (${rollback} / 100))`);
