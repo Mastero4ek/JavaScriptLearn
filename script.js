d63368122e6f8679d092d79369ce99640281b683
Chirkov Slava <slavachirkov92@gmail.com>
2023-02-01 16:24:46 +0300

    completed the task04

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let title = prompt("Как называется ваш проект"),
+let title = getTitle(prompt("Как называется ваш проект")),
 	screens = prompt("Какие типы экранов нужно разработать?", "Простые / Сложные / Интерактивные"),
 	screenPrice = +prompt("Сколько будет стоить данная работа?", "12000"),
 	service1 = prompt("Какой дополнительный тип услуги нужен?"),
@@ -6,34 +6,59 @@ let title = prompt("Как называется ваш проект"),
 	service2 = prompt("Какой дополнительный тип услуги нужен?"),
 	servicePrice2 = +prompt("Сколько это будет стоить?"),
 	adaptive = confirm("Нужен ли адаптив на сайте?");
-	
-const fullPrice = screenPrice + servicePrice1 + servicePrice2,
+
+const showTypeOf = function (variable) {
+	console.log(variable, typeof variable);
+}
+
+const getRollbackMessage = function(price) {
+	switch(true) {
+		case price >= 30000:
+			return "Даем скидку в 10%"
+			break
+
+		case price >= 15000 && price < 30000:
+			return "Даем скидку в 5%"
+			break
+
+		case price < 15000 && price >= 0:
+			return "Скидка не предусмотрена"
+			break
+			
+		default:
+			return "Что то пошло не так"
+			break
+	}
+}
+
+const getAllServicePrices = function(price1, price2) {
+	return price1 + price2
+}
+
+function getFullPrice(price1, price2) {
+	return price1 + price2
+}
+
+function getServicePercentPrices(price1, price2) {
+	return price1 - (price1 * (price2 / 100))
+}
+
+function getTitle(str) {
+	let strNew = str.trim();
+
+	return strNew.charAt(0).toUpperCase() + strNew.slice(1).toLowerCase()
+	//return title.charAt(0).toUpperCase() + title.slice(1).toLowerCase()
+}
+
+const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2),
+	fullPrice = getFullPrice(screenPrice, allServicePrices),
 	rollback = 75,
 	servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
 
-console.log(typeof title, typeof fullPrice, typeof adaptive);
-console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
 console.log(screens.toLowerCase().split(", "));
-console.log(`Процент отката посреднику за работу (${fullPrice} * (${rollback} / 100))`);
-
-console.log(Math.ceil(servicePercentPrice));
-
-switch(true) {
-	case fullPrice >= 30000:
-		console.log("Даем скидку в 10%");
-		break
-
-	case fullPrice >= 15000 && fullPrice < 30000:
-		console.log("Даем скидку в 5%");
-		break
-
-	case fullPrice < 15000 && fullPrice >= 0:
-		console.log("Скидка не предусмотрена");
-		break
-		
-	default:
-		console.log("Что то пошло не так");
-		break
-}
+console.log(getRollbackMessage(fullPrice));
+console.log(getServicePercentPrices(fullPrice, rollback));
